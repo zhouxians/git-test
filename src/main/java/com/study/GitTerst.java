201818047afa1330fc0d74d2e517da1fe67e2478
Xiaodaishushu <Xiaodaishushu@qq.com>
2021-07-12 14:11:15 +0800

    first commit

--- src/main/java/com/study/GitTerst.java
+++ src/main/java/com/study/GitTerst.java
@@ -3,6 +3,7 @@ package com.study;
 public class GitTerst {
     public static void main(String[] args) {
         System.out.println("hello Git !!");
+        System.out.println("hello Zhou jie !!");
     }
 }
 
