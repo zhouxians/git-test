474a6dccf13b3365f7d1e17d2d554332f3ee3359
Xiaodaishushu <Xiaodaishushu@qq.com>
2021-09-17 23:18:30 +0800

    第一次提交到分支

--- src/main/java/com/study/GitTerst.java
+++ src/main/java/com/study/GitTerst.java
@@ -4,6 +4,7 @@ public class GitTerst {
     public static void main(String[] args) {
         System.out.println("hello Git !!");
         System.out.println("hello Zhou jie !!");
+        System.out.println("本地dev push上去");
     }
 }
 
